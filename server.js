19d47f55587a49c8aa0d7e835c65f330c046bc34
unknown <salman.alamin01@gmail.com>
2022-11-11 12:00:14 -0800

    fixing port casing

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express();
 
-const port = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -64,6 +64,6 @@ app.get('api/notes/:id', (req, res) =>{
 
 
 
-app.listen(port, () => {
-    console.log(`listening on port: ${port}`);
+app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`);
 });
